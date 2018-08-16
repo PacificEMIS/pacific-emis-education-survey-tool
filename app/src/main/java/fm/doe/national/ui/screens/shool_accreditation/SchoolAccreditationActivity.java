7f850b014d2b1ced23641498210a3d6a1b4555a7
Alexander Chibirev <alexander.chibirev@omega-r.com>
2018-08-16 05:44:27 -0700

    added new theme, new toolbar, item school

--- app/src/main/java/fm/doe/national/ui/screens/shool_accreditation/SchoolAccreditationActivity.java
+++ app/src/main/java/fm/doe/national/ui/screens/shool_accreditation/SchoolAccreditationActivity.java
@@ -3,6 +3,9 @@ package fm.doe.national.ui.screens.shool_accreditation;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
@@ -35,6 +38,23 @@ public class SchoolAccreditationActivity extends MenuDrawerActivity implements S
     @Override
     protected MenuDrawerPresenter getPresenter() {
         return schoolAccreditationPresenter;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 
 }
