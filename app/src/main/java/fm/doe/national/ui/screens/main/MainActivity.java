40105f6e74a3ed5baee7c84061bde9d0a5378c14
Nikita Kuzin <nikita.kuzin@omega-r.com>
2018-08-17 14:37:17 +0300

    refactoring

--- app/src/main/java/fm/doe/national/ui/screens/main/MainActivity.java
+++ app/src/main/java/fm/doe/national/ui/screens/main/MainActivity.java
@@ -12,24 +12,9 @@ import fm.doe.national.ui.screens.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
-    @Inject
-    OrmLiteDataSource ormLiteDataSource;
-
     @Override
     protected int getContentView() {
         return R.layout.activity_main;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        MicronesiaApplication.getAppComponent().inject(this);
-        ormLiteDataSource.createSchool("TestSchool2", "id2").subscribe(school -> {
-            ormLiteDataSource.createNewSchoolAccreditation(2018, school).subscribe(schoolAccreditation -> {
-                ormLiteDataSource.requestSchoolAccreditationResults().subscribe(results -> {
-                    System.out.println();
-                });
-            });
-        }).dispose();
-    }
 }
