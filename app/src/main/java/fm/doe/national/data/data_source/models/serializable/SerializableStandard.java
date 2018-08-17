64bd0f0e12402ad1652186c155c5d7b3523d548e
Nikita Kuzin <nikita.kuzin@omega-r.com>
2018-08-17 15:28:06 +0300

    refactoring

--- app/src/main/java/fm/doe/national/data/data_source/models/serializable/SerializableStandard.java
+++ app/src/main/java/fm/doe/national/data/data_source/models/serializable/SerializableStandard.java
@@ -1,6 +1,7 @@
 package fm.doe.national.data.data_source.models.serializable;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -17,6 +18,7 @@ public class SerializableStandard implements Standard {
     @SerializedName("criteria")
     private Collection<SerializableCriteria> criterias;
 
+    @Nullable
     @Override
     public GroupStandard getGroupStandard() {
         return null;
