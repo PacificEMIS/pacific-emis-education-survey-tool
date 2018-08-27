71ee2683311aa1028b4852993446da8efd9d9983
Nikita Ivanov <nikita.ivanov@omega-r.com>
2018-08-27 14:16:46 +0300

    refactor

--- app/src/main/java/fm/doe/national/ui/screens/main/MainActivity.java
+++ app/src/main/java/fm/doe/national/ui/screens/main/MainActivity.java
@@ -1,7 +1,5 @@
 package fm.doe.national.ui.screens.main;
 
-import android.os.Bundle;
-
 import com.arellomobile.mvp.presenter.InjectPresenter;
 
 import fm.doe.national.R;
@@ -10,11 +8,6 @@ import fm.doe.national.ui.screens.base.BaseActivity;
 public class MainActivity extends BaseActivity {
     @InjectPresenter
     MainPresenter presenter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected int getContentView() {
