3bc057f9028565fec04119731f4018a37bbc87a2
Nikita Ivanov <nikita.ivanov@omega-r.com>
2018-09-10 18:08:51 +0300

    == changed to equals()

--- app/src/main/java/fm/doe/national/utils/CollectionUtils.java
+++ app/src/main/java/fm/doe/national/utils/CollectionUtils.java
@@ -9,7 +9,7 @@ public class CollectionUtils {
     @Nullable
     public static <K, V> K getKeyByValue(Map<K, V> map, V value) {
         for (Map.Entry<K, V> entry : map.entrySet()) {
-            if (entry.getValue() == value) {
+            if (entry.getValue().equals(value)) {
                 return entry.getKey();
             }
         }
