c0b655fc6270adcac59239676343edc56ba5bc46
Nikita Ivanov <nikita.ivanov@omega-r.com>
2018-09-14 12:21:46 +0300

    fixed bug #55944 Splash very long displayed on Samsung tablet

--- app/src/main/java/fm/doe/national/ui/screens/splash/SplashPresenter.java
+++ app/src/main/java/fm/doe/national/ui/screens/splash/SplashPresenter.java
@@ -3,21 +3,20 @@ package fm.doe.national.ui.screens.splash;
 import android.content.res.AssetManager;
 
 import com.arellomobile.mvp.InjectViewState;
-import com.omega_r.libs.omegatypes.Text;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import fm.doe.national.BuildConfig;
 import fm.doe.national.MicronesiaApplication;
-import fm.doe.national.R;
 import fm.doe.national.data.data_source.DataSource;
 import fm.doe.national.data.data_source.models.School;
 import fm.doe.national.data.data_source.models.serializable.LinkedSchoolAccreditation;
 import fm.doe.national.data.parsers.Parser;
 import fm.doe.national.ui.screens.menu.base.MenuPresenter;
+import fm.doe.national.utils.Constants;
 import io.reactivex.Completable;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -28,34 +27,42 @@ public class SplashPresenter extends MenuPresenter<SplashView> {
             MicronesiaApplication.getAppComponent().getSchoolAccreditationParser();
     private final Parser<List<School>> schoolsParser = MicronesiaApplication.getAppComponent().getSchoolsParser();
     private final DataSource dataSource = MicronesiaApplication.getAppComponent().getDataSource();
+    
+    private boolean isFinishedLoading;
 
     public SplashPresenter(AssetManager assetManager) {
         loadAssets(assetManager);
+        scheduleShowingProgress();
     }
 
     private void loadAssets(AssetManager assetManager) {
-        try {
-            InputStream inputStream = assetManager.open(BuildConfig.SURVEYS_FILE_NAME);
-            LinkedSchoolAccreditation schoolAccreditation = schoolAccreditationParser.parse(inputStream);
+        addDisposable(Single.fromCallable(() -> schoolAccreditationParser.parse(assetManager.open(BuildConfig.SURVEYS_FILE_NAME)))
+                .flatMapCompletable(schoolAccreditation -> Single.fromCallable(() ->
+                    schoolsParser.parse(assetManager.open(BuildConfig.SCHOOLS_FILE_NAME)))
+                        .flatMapCompletable(schools -> dataSource.requestSchools()
+                                    .flatMapCompletable(resultList -> {
+                                        if (resultList.isEmpty()) {
+                                            return dataSource
+                                                    .addSchools(schools)
+                                                    .andThen(dataSource.createSchoolAccreditation(schoolAccreditation));
+                                        }
+                                        return Completable.complete(); // already exists
+                                    })))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    isFinishedLoading = true;
+                    getViewState().showSelector();
+                    getViewState().hideLongLoadingProgressBar();
+                } , this::handleError));
+    }
 
-            inputStream = assetManager.open(BuildConfig.SCHOOLS_FILE_NAME);
-            List<School> schools = schoolsParser.parse(inputStream);
-
-            addDisposable(dataSource.requestSchools()
-                    .flatMapCompletable(resultList -> {
-                        if (resultList.isEmpty()) {
-                            return dataSource
-                                    .addSchools(schools)
-                                    .andThen(dataSource.createSchoolAccreditation(schoolAccreditation));
-                        }
-                        return Completable.complete(); // already exists
-                    })
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(getViewState()::showSelector, this::handleError));
-
-        } catch (IOException | Parser.ParseException ex) {
-            getViewState().showWarning(Text.from(R.string.title_warning), Text.from(R.string.warn_failed_to_load_assets));
-        }
+    private void scheduleShowingProgress() {
+        addDisposable(Completable.complete()
+                .delay(Constants.SECONDS_TO_LONG_LOADING_INDICATION, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    if (!isFinishedLoading) getViewState().showLongLoadingProgressBar();
+                }));
     }
 }
