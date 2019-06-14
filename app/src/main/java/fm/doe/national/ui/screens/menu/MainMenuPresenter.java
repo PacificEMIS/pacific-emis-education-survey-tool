a37df2cd98cea24a36bb2b8277eb619ca0156322
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-06-14 11:47:34 +0300

    setting app icon and name

--- app/src/main/java/fm/doe/national/ui/screens/menu/MainMenuPresenter.java
+++ app/src/main/java/fm/doe/national/ui/screens/menu/MainMenuPresenter.java
@@ -1,8 +1,12 @@
 package fm.doe.national.ui.screens.menu;
 
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
 
+import com.omega_r.libs.omegatypes.Image;
 import com.omega_r.libs.omegatypes.Text;
+import com.omega_r.libs.omegatypes.UrlImageExtensionsKt;
 import com.omegar.mvp.InjectViewState;
 
 import fm.doe.national.R;
@@ -20,6 +24,20 @@ public class MainMenuPresenter extends BasePresenter<MainMenuView> {
 
     public MainMenuPresenter() {
         getViewState().setCurrentSurveyType(globalPreferences.getSurveyType());
+    }
+
+    @Override
+    public void attachView(MainMenuView view) {
+        super.attachView(view);
+
+        String logoPath = globalPreferences.getLogoPath();
+
+        if (logoPath != null) {
+            getViewState().setIcon(UrlImageExtensionsKt.from(Image.Companion, logoPath));
+        }
+
+        String appName = globalPreferences.getAppName();
+        getViewState().setTitle(TextUtils.isEmpty(appName) ? Text.from(R.string.app_name) : Text.from(appName));
     }
 
     public void onCreditsPressed() {
