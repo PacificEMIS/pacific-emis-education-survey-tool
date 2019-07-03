5c290b4e12950ad1d9da7a1e14d533fa534be48a
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-07-03 16:32:58 +0300

    devices screen design

--- offline_sync/src/main/java/fm/doe/national/offline_sync/ui/devices/PairedDevicesAdapter.java
+++ offline_sync/src/main/java/fm/doe/national/offline_sync/ui/devices/PairedDevicesAdapter.java
@@ -1,9 +1,11 @@
 package fm.doe.national.offline_sync.ui.devices;
 
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.omega_r.libs.omegarecyclerview.BaseListAdapter;
 
@@ -11,6 +13,8 @@ import fm.doe.national.offline_sync.R;
 import fm.doe.national.offline_sync.data.model.Device;
 
 public class PairedDevicesAdapter extends BaseListAdapter<Device> {
+
+    private int selectedPosition = RecyclerView.NO_POSITION;
 
     public PairedDevicesAdapter(@Nullable OnItemClickListener<Device> clickListener) {
         super(clickListener);
@@ -34,6 +38,27 @@ public class PairedDevicesAdapter extends BaseListAdapter<Device> {
         protected void onBind(Device item) {
             nameTextView.setText(item.getName() == null ? getString(R.string.title_no_name) : item.getName());
             macTextView.setText(item.getAddress());
+
+            itemView.setActivated(isSelected());
+        }
+
+        private boolean isSelected() {
+            return selectedPosition == getAdapterPosition();
+        }
+
+        @Override
+        public void onClick(View v) {
+            super.onClick(v);
+
+            if (isSelected()) {
+                selectedPosition = RecyclerView.NO_POSITION;
+                notifyItemChanged(getAdapterPosition());
+            } else {
+                int oldSelectedPosition = selectedPosition;
+                selectedPosition = getAdapterPosition();
+                notifyItemChanged(oldSelectedPosition);
+                notifyItemChanged(selectedPosition);
+            }
         }
     }
 }
