bc2b95788e2d056fbcbe8d5dbcfb9caae9d5ad15
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-07-15 17:28:20 +0300

    cr2

--- remote_storage/src/main/java/fm/doe/national/remote_storage/data/uploader/UploadWorker.java
+++ remote_storage/src/main/java/fm/doe/national/remote_storage/data/uploader/UploadWorker.java
@@ -44,8 +44,8 @@ public class UploadWorker extends RxWorker {
         })
                 .flatMap(dataSource::loadSurvey)
                 .flatMapCompletable(survey -> remoteStorage.upload(survey))
-                .onErrorResumeNext(t -> {
-                    t.printStackTrace();
+                .onErrorResumeNext(throwable -> {
+                    throwable.printStackTrace();
                     remoteStorageAccessor.scheduleUploading(surveyId);
                     return Completable.complete();
                 })
