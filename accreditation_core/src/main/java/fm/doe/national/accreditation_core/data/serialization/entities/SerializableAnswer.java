5c091003f8dd9a28cd808297669a3c2f6a51a6aa
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-08-07 15:51:55 +0300

    serializing fixed

--- accreditation_core/src/main/java/fm/doe/national/accreditation_core/data/serialization/entities/SerializableAnswer.java
+++ accreditation_core/src/main/java/fm/doe/national/accreditation_core/data/serialization/entities/SerializableAnswer.java
@@ -4,13 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import fm.doe.national.accreditation_core.data.model.Answer;
 import fm.doe.national.accreditation_core.data.model.AnswerState;
 import fm.doe.national.core.data.model.Photo;
+import fm.doe.national.core.data.serialization.model.SerializablePhoto;
 
 @Root(name = "answer")
 public class SerializableAnswer implements Answer {
@@ -22,12 +25,20 @@ public class SerializableAnswer implements Answer {
     @Element(required = false)
     String comment;
 
+    @Nullable
+    @ElementList(entry = "photo", required = false, inline = true)
+    List<SerializablePhoto> photos;
+
     public SerializableAnswer() {
     }
 
     public SerializableAnswer(@NonNull Answer otherAnswer) {
         this.state = otherAnswer.getState();
         this.comment = otherAnswer.getComment();
+
+        if (otherAnswer.getPhotos() != null) {
+            this.photos = otherAnswer.getPhotos().stream().map(SerializablePhoto::new).collect(Collectors.toList());
+        }
     }
 
     @Override
@@ -49,6 +60,6 @@ public class SerializableAnswer implements Answer {
     @Nullable
     @Override
     public List<? extends Photo> getPhotos() {
-        return null;
+        return photos;
     }
 }
