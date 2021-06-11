91ecc1ec398525e95cba47c87b3a6b2584b6d5ef
ZXeRRinor <zxerrinor@gmail.com>
2021-06-11 15:51:37 +0300

    Created class-constructor with API-client initializer, using ApiContext-enum.

--- remote_data/src/main/java/org/pacific_emis/surveys/remote_data/Network.java
+++ remote_data/src/main/java/org/pacific_emis/surveys/remote_data/Network.java
@@ -1,14 +1,25 @@
 package org.pacific_emis.surveys.remote_data;
 
+import java.io.IOException;
+import java.util.List;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Network {
-    public static FedEmisApi initFedEmis() {
+    EmisApi fedEmisApi;
+    EmisApi miEmisApi;
+
+    public Network() {
+        fedEmisApi = initEmisApi(ApiContext.FEDEMIS);
+        miEmisApi = initEmisApi(ApiContext.MIEMIS);
+    }
+
+    private EmisApi initEmisApi(ApiContext context) {
         return new Retrofit.Builder()
-                .baseUrl("https://fedemis.doe.fm/api")
+                .baseUrl(context.baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
-                .create(FedEmisApi.class);
+                .create(EmisApi.class);
     }
 }
