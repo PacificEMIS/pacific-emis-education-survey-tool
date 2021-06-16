3bde2c39b98e1efa0808988b22a421d5b2fddb3f
ZXeRRinor <zxerrinor@gmail.com>
2021-06-16 17:08:11 +0300

    Extracted strings to constants, using RxJava instead of Thread and Runnable.

--- app/src/main/java/org/pacific_emis/surveys/ui/screens/settings/SettingsPresenter.java
+++ app/src/main/java/org/pacific_emis/surveys/ui/screens/settings/SettingsPresenter.java
@@ -28,6 +28,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import io.reactivex.Completable;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -36,6 +38,9 @@ public class SettingsPresenter extends BasePresenter<SettingsView> {
 
     private final static String MIME_TYPE_SCHOOLS = "text/csv";
     private final static String MIME_TYPE_CERTIFICATE = "application/octet-stream";
+    private final static String FSM_REGION_NAME = "FSM";
+    private final static String RMI_REGION_NAME = "RMI";
+    private final static String SCHOOL_TABLE_HEADER = "schNo,schName\n";
 
     private final SettingsInteractor interactor = MicronesiaApplication.getInjection().getAppComponent().getSettingsInteractor();
     private final LocalSettings localSettings = MicronesiaApplication.getInjection().getCoreComponent().getLocalSettings();
@@ -227,52 +232,66 @@ public class SettingsPresenter extends BasePresenter<SettingsView> {
     }
 
     private void onLoadSchoolsPressed() {
-        SettingsPresenter presenter = this;
-        Runnable runnable = new Runnable() {
-            public void run() {
-                try {
-                    Network agent = new Network();
-                    AppRegion region = localSettings.getAppRegion();
-                    ApiContext apiContext = null;
-                    String regionName = "";
-                    String content = null;
-                    switch (region) {
-                        case FSM:
-                            apiContext = ApiContext.FEDEMIS;
-                            regionName = "FSM";
-                            break;
-                        case RMI:
-                            apiContext = ApiContext.MIEMIS;
-                            regionName = "RMI";
-                            break;
-                    }
-                    if (apiContext != null) {
-                        List<School> schools = agent.getListOfSchoolNamesAndCodesFrom(apiContext);
-                        StringBuilder contentBuilder = new StringBuilder("schNo,schName\n");
-                        contentBuilder = contentBuilder.append(regionName).append(",").append(regionName).append("\n");
-                        for (School school : schools) {
-                            contentBuilder.append(school.code).append(",").append(school.name).append("\n");
-                        }
-                        content = contentBuilder.toString();
-                    }
-                    if (content != null) {
-                        addDisposable(interactor.importSchools(content)
-                                .subscribeOn(Schedulers.io())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .doOnSubscribe(disposable -> getViewState().showWaiting())
-                                .doFinally(() -> getViewState().hideWaiting())
-                                .subscribe(
-                                        () -> getViewState().showToast(Text.from(R.string.toast_load_schools_success)),
-                                        presenter::handleError
-                                ));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        addDisposable(loadSchoolsFromApi()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> { /* do nothing */ }, this::handleError));
+    }
+
+    private Completable loadSchoolsFromApi() {
+        return Single.fromCallable(() -> {
+            try {
+                Network agent = new Network();
+                AppRegion region = localSettings.getAppRegion();
+                ApiContext apiContext = null;
+                String regionName = "";
+                String content = null;
+
+                switch (region) {
+                    case FSM:
+                        apiContext = ApiContext.FEDEMIS;
+                        regionName = FSM_REGION_NAME;
+                        break;
+                    case RMI:
+                        apiContext = ApiContext.MIEMIS;
+                        regionName = RMI_REGION_NAME;
+                        break;
                 }
+
+                if (apiContext != null) {
+                    List<School> schools = agent.getListOfSchoolNamesAndCodesFrom(apiContext);
+                    StringBuilder contentBuilder = new StringBuilder(SCHOOL_TABLE_HEADER);
+                    contentBuilder = contentBuilder
+                            .append(regionName)
+                            .append(",")
+                            .append(regionName)
+                            .append("\n");
+                    for (School school : schools) {
+                        contentBuilder
+                                .append(school.code)
+                                .append(",")
+                                .append(school.name)
+                                .append("\n");
+                    }
+                    content = contentBuilder.toString();
+                }
+
+                if (content != null) {
+                    addDisposable(interactor.importSchools(content)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnSubscribe(disposable -> getViewState().showWaiting())
+                            .doFinally(() -> getViewState().hideWaiting())
+                            .subscribe(
+                                    () -> getViewState().showToast(Text.from(R.string.toast_load_schools_success)),
+                                    ((SettingsPresenter) this)::handleError
+                            ));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        };
-        Thread thread = new Thread(runnable);
-        thread.start();
+            return 0; // something not null. Doesn't means, what will be returned, because it will be ignored at next line.
+        }).ignoreElement();
     }
 
     @Override
