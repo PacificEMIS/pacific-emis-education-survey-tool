8668919b401be5273cf0769abf1bdd65fd395923
ZXeRRinor <zxerrinor@gmail.com>
2021-06-17 14:39:08 +0300

    Fixed incorrect parameter in requests to EMIS API.

--- remote_data/src/main/java/org/pacific_emis/surveys/remote_data/EmisApi.java
+++ remote_data/src/main/java/org/pacific_emis/surveys/remote_data/EmisApi.java
@@ -11,15 +11,16 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 interface EmisApi {
     @GET("schools")
-    Call<List<School>> getSchools(@Query("Authorization") String token);
+    Call<List<School>> getSchools(@Query("PageNo") int page, @Query("PageSize") int pageSize, @Header("Authorization") String token);
 
     @GET("teachers")
-    Call<Teachers> getTeachers(@Query("PageNo") int page, @Query("PageSize") int pageSize, @Query("Authorization") String token);
+    Call<Teachers> getTeachers(@Query("PageNo") int page, @Query("PageSize") int pageSize, @Header("Authorization") String token);
 
     @GET("lookups/collection/core")
     Call<Core> getCore();
