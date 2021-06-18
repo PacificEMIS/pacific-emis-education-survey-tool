384c2ec3fc2fedad2868ab7d19c4c9ed9d4ecaf6
ZXeRRinor <zxerrinor@gmail.com>
2021-06-18 18:04:25 +0300

    Method for loading all subjects to DataSource-interface.

--- core/src/main/java/org/pacific_emis/surveys/core/data/data_source/DataSource.java
+++ core/src/main/java/org/pacific_emis/surveys/core/data/data_source/DataSource.java
@@ -19,6 +19,8 @@ public interface DataSource {
 
     Single<List<Teacher>> loadTeachers();
 
+    Single<List<Subject>> loadSubjects();
+
     Completable rewriteAllSchools(List<School> schools);
 
     Completable rewriteAllTeachers(List<Teacher> schools);
