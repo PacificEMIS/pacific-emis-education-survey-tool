24a9d8c0dcf6f35d72f53756eecd9b11b345d1a4
ZXeRRinor <zxerrinor@gmail.com>
2021-06-18 15:45:05 +0300

    Added Subject-DAO to DB-class of application.

--- core/src/main/java/org/pacific_emis/surveys/core/data/persistence/SchoolInfoDatabase.java
+++ core/src/main/java/org/pacific_emis/surveys/core/data/persistence/SchoolInfoDatabase.java
@@ -5,14 +5,17 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 import org.pacific_emis.surveys.core.data.persistence.dao.SchoolDao;
+import org.pacific_emis.surveys.core.data.persistence.dao.SubjectDao;
 import org.pacific_emis.surveys.core.data.persistence.dao.TeacherDao;
 import org.pacific_emis.surveys.core.data.persistence.model.RoomSchool;
+import org.pacific_emis.surveys.core.data.persistence.model.RoomSubject;
 import org.pacific_emis.surveys.core.data.persistence.model.RoomTeacher;
 
 @Database(
         entities = {
                 RoomSchool.class,
-                RoomTeacher.class
+                RoomTeacher.class,
+                RoomSubject.class
         },
         version = 1,
         exportSchema = false)
@@ -23,5 +26,6 @@ public abstract class SchoolInfoDatabase extends RoomDatabase {
 
     public abstract SchoolDao getSchoolDao();
     public abstract TeacherDao getTeacherDao();
+    public abstract SubjectDao getSubjectDao();
 
 }
