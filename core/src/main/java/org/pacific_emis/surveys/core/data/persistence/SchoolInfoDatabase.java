d45f9b26385304a706a11ab36bdb9aa9f71f323b
ZXeRRinor <zxerrinor@gmail.com>
2021-07-05 14:59:46 +0300

    Set exportSchema to true for testing of migrations for SchoolInfoDatabase.

--- core/src/main/java/org/pacific_emis/surveys/core/data/persistence/SchoolInfoDatabase.java
+++ core/src/main/java/org/pacific_emis/surveys/core/data/persistence/SchoolInfoDatabase.java
@@ -19,8 +19,8 @@ import org.pacific_emis.surveys.core.data.persistence.model.RoomTeacher;
                 RoomTeacher.class,
                 RoomSubject.class
         },
-        version = 3,
-        exportSchema = false)
+        version = 2,
+        exportSchema = true)
 @TypeConverters({
         BaseConverters.class
 })
@@ -37,16 +37,6 @@ public abstract class SchoolInfoDatabase extends RoomDatabase {
                     "CREATE TABLE IF NOT EXISTS RoomTeacher (id INTEGER NOT NULL, name TEXT, appRegion TEXT, PRIMARY KEY(id));"
             );
             database.execSQL("CREATE INDEX IF NOT EXISTS index_RoomTeacher_id ON RoomTeacher (id);");
-        }
-    };
-
-    public static final Migration MIGRATION_2_3 = new Migration(2, 3) {
-        @Override
-        public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL(
-                    "CREATE TABLE IF NOT EXISTS RoomSubject (id TEXT NOT NULL, name TEXT, appRegion TEXT, PRIMARY KEY(id));"
-            );
-            database.execSQL("CREATE INDEX IF NOT EXISTS index_RoomSubject_id ON RoomSubject (id);");
         }
     };
 }
