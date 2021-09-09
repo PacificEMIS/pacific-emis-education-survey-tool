481c4f2badc1c93429d4658d8928738ca604c4b4
Ksenia Yunknikel <ksenia.yunknickel@omega-r.com>
2021-09-09 10:28:41 +0300

    Added emis_user and emis_password in RemoteDataSourse

--- core/src/main/java/org/pacific_emis/surveys/core/di/modules/RemoteDataSourceModule.java
+++ core/src/main/java/org/pacific_emis/surveys/core/di/modules/RemoteDataSourceModule.java
@@ -13,7 +13,7 @@ public class RemoteDataSourceModule {
     @Provides
     @CoreScope
     public CoreRemoteDataSource provideRemoteDataSource(LocalSettings localSettings) {
-        return CoreRemoteDataSource.createByAppRegion(localSettings.getAppRegion());
+        return CoreRemoteDataSource.createByAppRegion(localSettings.getAppRegion(), localSettings.getEmisUser(), localSettings.getEmisPassword());
     }
 
 }
